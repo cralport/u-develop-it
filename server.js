146bac7445c097b829796bcbbb5b70637901177e
cory alport <cralport@gmail.com>
2021-04-06 17:45:08 -0600

    added dbquery statements

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const e = require('express');
 const express =  require('express');
 const PORT = process.env.PORT || 3001;
 const mysql = require('mysql2');
@@ -19,9 +20,37 @@ const db = mysql.createConnection(
     },
     console.log('Connected to the election database.')
 );
-db.query(`SELECT * FROM candidates`, (err, rows) =>{
-    console.log(rows);
-});
+// db.query(`SELECT * FROM candidates`, (err, rows) =>{
+//     console.log(rows);
+// });
+
+//Get a single candidate
+// db.query('SELECT * FROM candidates WHERE id = 1', (err, row) =>{
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(row);
+// });
+
+//Delete a candidate
+// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, results) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(results);
+// }); 
+
+//create a candidate
+// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+//                 VALUES (?, ?, ?, ?)`;
+// const params = [1, 'Ronald', 'Firbank', 1];
+
+// db.query(sql, params, (err, result) => {
+//     if (err); {
+//         console.log(err);
+//     }
+//     console.log(result);
+// });
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
