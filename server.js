713285bc30a385a8e52c126be435404555d2b61c
cory alport <cralport@gmail.com>
2021-04-06 17:26:01 -0600

    added createConnection

--- server.js
+++ server.js
@@ -1,13 +1,27 @@
 const express =  require('express');
 const PORT = process.env.PORT || 3001;
+const mysql = require('mysql2');
 const app = express();
 //Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 
-
-
+//Connect to datatbase
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        //Your MySQL Username,
+        user: 'root',
+        //Your MySQL Password
+        password: 'Fight4er!1207',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+db.query(`SELECT * FROM candidates`, (err, rows) =>{
+    console.log(rows);
+});
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
